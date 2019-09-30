dc3a92729ba68f6698faf0cd0acc661f94516aa2
Augusto Alonso de la Cruz Jimenez <augustoalonso.delacruz@bbva.com>
2019-09-30 13:35:12 -0500

    Definiendo método en clase para pintar en consola

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import _ from 'lodash';
+//import _ from 'lodash';
 import printMe from './print.js';
+import BbvaButton from '@bbva-web-components/bbva-button-default';
 
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack   '], ' ');
+  const bbvaButton = document.createElement('bbva-button-default');
+  bbvaButton.text = 'BBVA Button into a Webpack module';
+  bbvaButton.onclick =  printMe;
+
+  //element.innerHTML = _.join(['Hello', 'webpack   '], ' ');
+  element.innerHTML = ['Hello', 'webpack'].join(' - ');
 
   btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  btn.onclick = greeting;
 
   element.appendChild(btn);
+  element.appendChild(bbvaButton);
 
   return element;
 }
 
+function greeting() {
+  console.log('Hola desde componente Bbva');
+}
 document.body.appendChild(component());
